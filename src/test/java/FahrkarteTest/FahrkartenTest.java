e67a221d2dae59a0b66bac90ec243b7ccf4da4cd
Benedict <enepene@hotmail.com>
2020-04-06 10:51:42 +0200

    Aktueller Stand. Versuche die gültige Bezahlung zu regeln.

---
+++ src/test/java/FahrkarteTest/FahrkartenTest.java
@@ -0,0 +1,30 @@
+package FahrkarteTest;
+
+import org.junit.Test;
+import fahrkartenautomat.Automat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+
+public class FahrkartenTest {
+
+    @Test
+    public void fkTest(){
+        Automat a = new Automat();
+        a.setPosition(1);
+        assertThat(a.getPosition(), is(1));
+        assertThat(a.calculatePrice(1), is(2.3));
+        assertThat(a.calculatePrice(2), is(4.6));
+        a.setPosition(5);
+        assertThat(a.calculatePrice(1), is(11.5));
+        a.setPosition(1);
+        assertThat(a.getPosition(), is (1));
+        assertThat(a.calculatePrice(1), is(2.3));
+        assertThat(a.getCashInput(), is(0.5));
+        assertThat(a.payment(), is(1.8));
+    }
+}
